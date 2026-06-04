dedaac0ae7a43c99285b8d41a167a55576ac16ee
naufaladyapradana <naufalkuy6@gmail.com>
2026-06-04 13:23:49 +0700

    Add files via upload

--- laundry_niwasa.sql
+++ laundry_niwasa.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jun 03, 2026 at 04:52 AM
+-- Generation Time: Jun 03, 2026 at 09:00 AM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
@@ -40,7 +40,24 @@ CREATE TABLE `detail_transaksi` (
 --
 
 INSERT INTO `detail_transaksi` (`id`, `transaksi_id`, `service_id`, `berat`, `subtotal`) VALUES
-(1, 1, 2, 1.00, 10000);
+(1, 1, 2, 1.00, 10000),
+(2, 2, 2, 1.00, 10000),
+(3, 2, 1, 1.00, 7000);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `ratings`
+--
+
+CREATE TABLE `ratings` (
+  `id` int(11) NOT NULL,
+  `transaksi_id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `rating` int(11) NOT NULL,
+  `komentar` varchar(255) DEFAULT NULL,
+  `tanggal` datetime DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
 
@@ -74,15 +91,17 @@ CREATE TABLE `transaksi` (
   `id` int(11) NOT NULL,
   `user_id` int(11) DEFAULT NULL,
   `total_harga` int(11) DEFAULT NULL,
-  `tanggal` datetime DEFAULT current_timestamp()
+  `tanggal` datetime DEFAULT current_timestamp(),
+  `status` varchar(20) DEFAULT 'Menunggu'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `transaksi`
 --
 
-INSERT INTO `transaksi` (`id`, `user_id`, `total_harga`, `tanggal`) VALUES
-(1, 1, 10000, '2026-06-03 08:49:30');
+INSERT INTO `transaksi` (`id`, `user_id`, `total_harga`, `tanggal`, `status`) VALUES
+(1, 1, 10000, '2026-06-03 08:49:30', 'Menunggu'),
+(2, 2, 17000, '2026-06-03 12:38:16', 'Menunggu');
 
 -- --------------------------------------------------------
 
@@ -93,15 +112,18 @@ INSERT INTO `transaksi` (`id`, `user_id`, `total_harga`, `tanggal`) VALUES
 CREATE TABLE `users` (
   `id` int(11) NOT NULL,
   `username` varchar(50) NOT NULL,
-  `password` varchar(255) NOT NULL
+  `password` varchar(255) NOT NULL,
+  `role` varchar(10) DEFAULT 'user'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `users`
 --
 
-INSERT INTO `users` (`id`, `username`, `password`) VALUES
-(1, 'sarah', '12345');
+INSERT INTO `users` (`id`, `username`, `password`, `role`) VALUES
+(1, 'sarah', '12345', 'admin'),
+(2, 'Opank', '102042430012', 'user'),
+(3, 'naufal', '102042430012', 'user');
 
 --
 -- Indexes for dumped tables
@@ -111,6 +133,12 @@ INSERT INTO `users` (`id`, `username`, `password`) VALUES
 -- Indexes for table `detail_transaksi`
 --
 ALTER TABLE `detail_transaksi`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `ratings`
+--
+ALTER TABLE `ratings`
   ADD PRIMARY KEY (`id`);
 
 --
@@ -140,7 +168,13 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `detail_transaksi`
 --
 ALTER TABLE `detail_transaksi`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT for table `ratings`
+--
+ALTER TABLE `ratings`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `services`
@@ -152,13 +186,13 @@ ALTER TABLE `services`
 -- AUTO_INCREMENT for table `transaksi`
 --
 ALTER TABLE `transaksi`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
